8d2b4cb2aa2d9c116b4c801890d5b09b4922fdae
Appru <j.appru@gmail.com>
2024-12-04 14:54:10 +0000

    login

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+import time
 
 print ("hello")
 
@@ -17,12 +19,21 @@ def get_drvier():
   options.add_argument("disable-blink-features=AutomationControlled")
 
   driver = webdriver.Chrome(service=service, options=options)
-  driver.get("http://automated.pythonanywhere.com")
+  driver.get("https://automated.pythonanywhere.com/login/")
   return driver
+
+def clean_text(text):
+    """extract only temp """
+    output = float(text.split(": ")[1])
+    return output
+
 
 def main():
   driver = get_drvier()
-  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
-  return element.text
+  driver.find_element(by="id", value="id_username").send_keys("automated")
+  driver.find_element(by = "id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+  time.sleep(3)
+  print(driver.current_url)
+ 
 
 print (main())
