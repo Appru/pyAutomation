c4000816ed516ab4728a7e2e09c687c621ebf027
Appru <j.appru@gmail.com>
2024-12-10 16:33:39 +0000

    lotsa files

--- pathlibIntro.py
+++ pathlibIntro.py
@@ -1,10 +1,19 @@
 from pathlib import Path
 
+def renameFiles():
+    root_dir = Path('data')
+    file_paths = root_dir.iterdir()
+    for path in file_paths:
+        new_filename = "new-" +path.stem +path.suffix
+        new_filepath = path.with_name(new_filename)
+        print(new_filepath)
+        path.rename(new_filepath)
+
+#renameFiles()
+
 root_dir = Path('data')
-file_paths = root_dir.iterdir()
+file_paths = root_dir.glob("**/*")
 
 for path in file_paths:
-    new_filename = "new-" +path.stem +path.suffix
-    new_filepath = path.with_name(new_filename)
-    print(new_filepath)
-    path.rename(new_filepath)
+    if path.is_file():
+        print(path)
