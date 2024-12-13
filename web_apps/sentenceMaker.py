c3bab37c1931a1543c5d1520ada6e4036f3f2d75
Appru <j.appru@gmail.com>
2024-12-13 16:41:48 +0000

    sentence maker lol

---
+++ web_apps/sentenceMaker.py
@@ -0,0 +1,8 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QLabel, QPushButton, QLineEdit
+
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle("Sentence Maker")
+window.show()
+app.exec()
